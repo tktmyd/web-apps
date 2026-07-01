896046003c6061fb505193f244fdbe0cd9f9a7df
Takuto Maeda <6821825+tktmyd@users.noreply.github.com>
2026-07-01 22:36:29 +0900

    Added space-time plot

--- src/group-speed.py
+++ src/group-speed.py
@@ -22,10 +22,10 @@ def _(np):
     ω = 2 * π * f
     λ = 40
     k = 2 * π / λ
-    U = 3.5
-    df = 1 / 25 / 2
+    U = 3
+    df = 1 / 25 / 2 
     dω = 2 * π * df
-    dk = dω / 2 / U
+    dk = dω / U
     return dk, dω, k, ω
 
 
@@ -33,7 +33,7 @@ def _(np):
 def _(dk, dω, k, np, plt, ω):
     def plot(x): 
 
-        t = np.linspace(0, 200, 801)
+        t = np.linspace(0, 300, 801)
         u0 = np.cos((k+dk/2) * x - (ω + dω/2) * t)
         u1 = np.cos((k-dk/2) * x - (ω - dω/2) * t)
         env = 2 * np.cos(dk/2 * x - dω/2 * t)
@@ -80,6 +80,42 @@ def _(mo, slider_x):
 @app.cell
 def _(plot, slider_x):
     plot(slider_x.value)
+    return
+
+
+@app.cell
+def _(dk, dω, k, np, ω):
+    z = np.zeros([1000, 800])
+    x = np.linspace(0, 1000, 1001)
+    t = np.linspace(0, 200, 801)
+
+    for n in range(len(t)-1):
+        for i in range(len(x)-1):
+            u0 = np.cos((k+dk/2) * x[i] - (ω + dω/2) * t[n])
+            u1 = np.cos((k-dk/2) * x[i] - (ω - dω/2) * t[n])
+            z[i,n] = u0 + u1
+    return t, x, z
+
+
+@app.cell
+def _(plt, t, x, z):
+    def plot3d(): 
+        fig = plt.figure(figsize=(6, 8))
+        ax = fig.add_subplot(1, 1, 1)
+    
+        p = ax.pcolormesh(t,x, z, shading='auto', cmap='Blues')
+        ax.set_xlabel("time [s]")
+        ax.set_ylabel("distance [km]")
+        fig.colorbar(p)
+
+        return fig
+
+    return (plot3d,)
+
+
+@app.cell
+def _(plot3d):
+    plot3d()
     return
 
 
